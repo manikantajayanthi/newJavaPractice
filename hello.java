95a3c731c7dfd3379eddd2c19e0940a9612ab2d4
Manikanta Jogarao Jayanthi <91141526+manikantajayanthi@users.noreply.github.com>
2023-11-18 13:17:50 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,6 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, batch 32 students.!!"); 
         System.out.println("Hello, Hi Mani.!!"); 
+        System.out.println("Hello, Hi Mani.!!"); 
         
     }
 }
